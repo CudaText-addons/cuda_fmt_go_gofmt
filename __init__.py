0626b6dd75966a78e44858f07cb3cf662c9944d7
Alexey <support@uvviewsoft.com>
2022-06-09 11:18:26 +0300

    fix

--- __init__.py
+++ __init__.py
@@ -1,8 +1,6 @@
 import os
 import subprocess
 
-MSG_CANNOT_RUN = "Cannot run 'gofmt'. Make sure it's in your PATH."
-
 def do_format(text):
 
     enc = 'utf-8'
@@ -10,22 +8,16 @@ def do_format(text):
         startupinfo = subprocess.STARTUPINFO()
         startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
         startupinfo.wShowWindow = subprocess.SW_HIDE
-        try:
-            p = subprocess.Popen(['gofmt'], 
-              startupinfo=startupinfo, 
-              stdout=subprocess.PIPE, 
-              stdin=subprocess.PIPE, 
-              stderr=subprocess.PIPE)
-        except OSError:
-            raise Exception(MSG_CANNOT_RUN)
+        p = subprocess.Popen(['gofmt'], 
+          startupinfo=startupinfo, 
+          stdout=subprocess.PIPE, 
+          stdin=subprocess.PIPE, 
+          stderr=subprocess.PIPE)
     else:
-        try:
-            p = subprocess.Popen(['gofmt'], 
-              stdout=subprocess.PIPE, 
-              stdin=subprocess.PIPE, 
-              stderr=subprocess.PIPE)
-        except OSError:
-            raise Exception(MSG_CANNOT_RUN)
+        p = subprocess.Popen(['gofmt'], 
+          stdout=subprocess.PIPE, 
+          stdin=subprocess.PIPE, 
+          stderr=subprocess.PIPE)
     
     stdout, stderr = p.communicate(text.encode(enc))
     if stdout:
